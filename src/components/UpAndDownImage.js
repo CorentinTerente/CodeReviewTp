3b3a7e7819e718d4a0dc1cdeea00856740056e2d
fcerfon <florent.cerfon@gmail.com>
2019-01-04 14:58:07 +0100

    The image is now doing a 'up and down' rather than a fading animation

--- src/components/UpAndDownImage.js
+++ src/components/UpAndDownImage.js
@@ -1,31 +1,45 @@
 import React, { Component } from 'react';
-import { Image, Animated } from 'react-native';
+import { Animated } from 'react-native';
 
 export default class UpAndDownImage extends Component {
   
   state = {
-    fadeAnim: new Animated.Value(0),
+    marginTop: new Animated.Value(0),
   }
 
   componentDidMount() {
+    this.down();
+  }
+
+  down() {
     Animated.timing(
-      this.state.fadeAnim,
+      this.state.marginTop,
       {
-        toValue: 1,
-        duration: 10000,
+        toValue: 200,
+        duration: 3000,
       }
-    ).start()
+    ).start(() => this.up())
+  }
+
+  up() {
+    Animated.timing(
+      this.state.marginTop,
+      {
+        toValue: 0,
+        duration: 3000,
+      }
+    ).start(() => this.down())
   }
 
   render() {
 
-    let { fadeAnim } = this.state;
+    let { marginTop } = this.state;
 
     return (
-      <Animated.Image source={require('../../assets/NY1.jpg')}
+      <Animated.Image
+        source={require('../../assets/NY1.jpg')}
         style={{
-          ...this.props.style,
-          opacity: fadeAnim, 
+          marginTop: marginTop, 
         }}
       >
       </Animated.Image>
